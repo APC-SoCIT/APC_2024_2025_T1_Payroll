786766c919808691de251774b9384c4c86403dc5
cgvillarroel <cgvillarroel@student.apc.edu.ph>
2024-09-28 17:01:32 +0800

    Add account management tests

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -1,85 +1,95 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-test('profile page is displayed', function () {
+uses(RefreshDatabase::class);
+
+test('accounts list page is restricted', function () {
     $user = User::factory()->create();
 
     $response = $this
         ->actingAs($user)
-        ->get('/profile');
+        ->get('/accounts');
+
+    $response->assertRedirect('/dashboard');
+});
+
+test('accounts list page is displayed', function () {
+    $user = User::factory()
+        ->authorized()
+        ->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->get('/account/1');
 
     $response->assertOk();
 });
 
-test('profile information can be updated', function () {
+test('account page is restricted', function () {
+    $user = User::factory()
+        ->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->get('/account/1');
+
+    $response->assertRedirect('/dashboard');
+});
+
+test('account page is displayed', function () {
+    $user = User::factory()
+        ->authorized()
+        ->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->get('/account/1');
+
+    $response->assertOk();
+});
+
+test('account update is restricted', function () {
     $user = User::factory()->create();
 
     $response = $this
         ->actingAs($user)
-        ->patch('/profile', [
-            'name' => 'Test User',
+        ->patch('/account/1', [
+            'name' => 'Test Account',
             'email' => 'test@example.com',
+            'active' => true,
+        ]);
+
+    $response
+        ->assertRedirect('/dashboard');
+
+    $this->assertDatabaseMissing('users', [
+        'name' => 'Test Account',
+    ]);
+});
+
+test('account can be updated', function () {
+    $user = User::factory()
+        ->authorized()
+        ->create();
+
+    $response = $this
+        ->actingAs($user)
+        ->patch('/account/1', [
+            'name' => 'Test Account',
+            'email' => 'test@example.com',
+            'active' => false,
         ]);
 
     $response
         ->assertSessionHasNoErrors()
-        ->assertRedirect('/profile');
+        ->assertRedirect('/accounts');
 
     $user->refresh();
 
-    $this->assertSame('Test User', $user->name);
-    $this->assertSame('test@example.com', $user->email);
-    $this->assertNull($user->email_verified_at);
-});
-
-test('email verification status is unchanged when the email address is unchanged', function () {
-    $user = User::factory()->create();
-
-    $response = $this
-        ->actingAs($user)
-        ->patch('/profile', [
-            'name' => 'Test User',
-            'email' => $user->email,
-        ]);
-
-    $response
-        ->assertSessionHasNoErrors()
-        ->assertRedirect('/profile');
-
-    $this->assertNotNull($user->refresh()->email_verified_at);
-});
-
-test('user can delete their account', function () {
-    $user = User::factory()->create();
-
-    $response = $this
-        ->actingAs($user)
-        ->delete('/profile', [
-            'password' => 'password',
-        ]);
-
-    $response
-        ->assertSessionHasNoErrors()
-        ->assertRedirect('/');
-
-    $this->assertGuest();
-    $this->assertNull($user->fresh());
-});
-
-test('correct password must be provided to delete account', function () {
-    $user = User::factory()->create();
-
-    $response = $this
-        ->actingAs($user)
-        ->from('/profile')
-        ->delete('/profile', [
-            'password' => 'wrong-password',
-        ]);
-
-    $response
-        ->assertSessionHasErrors('password')
-        ->assertRedirect('/profile');
-
-    $this->assertNotNull($user->fresh());
+    $this->assertDatabaseHas('users', [
+        'name' => 'Test Account',
+        'active' => false,
+    ]);
 });
