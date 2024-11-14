0ec8883a38aedc9cce4a044ead3b170eb2ee748f
unknown <jsalfafara@student.apc.edu.ph>
2024-11-15 00:34:04 +0800

    edited ItemAdditions and ItemDeductions

--- resources/js/Pages/Addition/ItemAdditions.vue
+++ resources/js/Pages/Addition/ItemAdditions.vue
@@ -1,5 +1,5 @@
 <script setup>
-import PrimaryButtom from '@/Components/PrimaryButton.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import UpdateItemAdditionForm from '@/Pages/Payroll/Partials/UpdateItemAdditionForm.vue';
 import { useFormat } from '@/Utils/FormatDate.js';
@@ -18,6 +18,7 @@ const form = useForm({
 </script>
 
 <template>
+
     <Head :title="addition.name + ' Additions'" />
 
     <AuthenticatedLayout>
@@ -28,29 +29,36 @@ const form = useForm({
         </template>
 
         <div class="py-12">
+
+            <!-- Select Account dropdown | Add button -->
+            <form @submit.prevent>
+                <div v-if="accountsWithout.length > 0" class="my-8 space-x-4 max-w-5xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                    <select class="border p-2 w-64" v-model="form.account_id">
+                        <option value="">Select account...</option>
+                        <option v-for="account in accountsWithout" :value="account.id">{{ account.name }}
+                        </option>
+                    </select>
+                    <PrimaryButton
+                        class="mr-10 m-5 rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                        type="button">
+                        <Link
+                            :href="route('itemAddition.new', { cutoff: cutoff.id, user: form.account_id, addition: addition.id })"
+                            method="post">+ Add
+                        </Link>
+                    </PrimaryButton>
+                </div>
+            </form>
+
+            <!-- Accounts with Salary/Wage Addition -->
             <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">Accounts with {{ addition.name }} Addition</h2>
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">Accounts with {{ addition.name }}
+                        Addition
+                    </h2>
 
-                    <form @submit.prevent>
-                        <select v-model="form.account_id">
-                            <option value="">Select account...</option>
-                            <option v-for="account in accountsWithout" :value="account.id">{{ account.name }}</option>
-                        </select>
-
-                        <PrimaryButton>
-                            <Link :href="route('itemAddition.new', { cutoff: cutoff.id, user: form.account_id, addition: addition.id})" method="post">
-                                Add
-                            </Link>
-                        </PrimaryButton>
-                    </form>
-                    <div v-for="itemAddition in itemAdditions">
-                        <UpdateItemAdditionForm
-                            :itemAddition
-                            :cutoff
-                            :additionView="true"
-                            :targetAccount="itemAddition.payroll_item.user"
-                        />
+                    <div class="my-8 w-4/6" v-for="itemAddition in itemAdditions">
+                        <UpdateItemAdditionForm :itemAddition :cutoff :additionView="true"
+                            :targetAccount="itemAddition.payroll_item.user" />
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Deduction/ItemDeductions.vue
+++ resources/js/Pages/Deduction/ItemDeductions.vue
@@ -17,6 +17,7 @@ const form = useForm({
 </script>
 
 <template>
+
     <Head :title="deduction.name + ' Deductions'" />
 
     <AuthenticatedLayout>
@@ -27,29 +28,34 @@ const form = useForm({
         </template>
 
         <div class="py-12">
+            <form @submit.prevent>
+                <div v-if="accountsWithout.length > 0" class="my-8 space-x-4 max-w-5xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                    <select class="border p-2 w-64" v-model="form.account_id">
+                        <option value="">Select account...</option>
+                        <option v-for="account in accountsWithout" :value="account.id">{{ account.name }}
+                        </option>
+                    </select>
+
+                    <PrimaryButton
+                        class="mr-10 m-5 rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                        type="button">
+                        <Link
+                            :href="route('itemDeduction.new', { cutoff: cutoff.id, user: form.account_id, deduction: deduction.id })"
+                            method="post">+ Add
+                        </Link>
+                    </PrimaryButton>
+                </div>
+            </form>
+
             <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">Accounts with {{ deduction.name }} Deduction</h2>
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-12">Accounts with {{ deduction.name }}
+                        Deduction
+                    </h2>
 
-                    <form @submit.prevent>
-                        <select v-model="form.account_id">
-                            <option value="">Select account...</option>
-                            <option v-for="account in accountsWithout" :value="account.id">{{ account.name }}</option>
-                        </select>
-
-                        <PrimaryButton>
-                            <Link :href="route('itemDeduction.new', { cutoff: cutoff.id, user: form.account_id, deduction: deduction.id})" method="post">
-                                Add
-                            </Link>
-                        </PrimaryButton>
-                    </form>
                     <div v-for="itemDeduction in itemDeductions">
-                        <UpdateItemDeductionForm
-                            :itemDeduction
-                            :cutoff
-                            :deductionView="true"
-                            :targetAccount="itemDeduction.payroll_item.user"
-                        />
+                        <UpdateItemDeductionForm :itemDeduction :cutoff :deductionView="true"
+                            :targetAccount="itemDeduction.payroll_item.user" />
                     </div>
                 </div>
             </div>
