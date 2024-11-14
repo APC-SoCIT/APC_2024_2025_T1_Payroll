5f6ad8426a469491003cd864ca33257d4a475888
Aliyah Kirstie Lopez <aclopez@student.apc.edu.ph>
2024-11-15 06:17:03 +0800

    remove hr access to indiv employee payroll list

--- resources/js/Components/Account.vue
+++ resources/js/Components/Account.vue
@@ -13,7 +13,7 @@ const props = defineProps([
         <tr >
             <td class="p-4 border-b border-blue-gray-50"></td>
             <td class="p-4 border-b border-blue-gray-50">
-                <p v-if="!($page.props.auth.isHr || $page.props.auth.isPayroll)" >{{ account.name }}</p>
+                <p v-if="!($page.props.auth.isPayroll)" >{{ account.name }}</p>
                 <Link v-else-if="cutoff == null" class="font-semibold underline text-gray-500 hover:text-gray-700derline" :href="route('cutoffs.getFromUser', account.id)">{{ account.name }}</Link>
                 <Link v-else class="font-semibold underline text-gray-500 hover:text-gray-700" :href="route('payroll.get', { cutoff: cutoff.id, user: account.id })">{{ account.name }}</Link>
             </td>
